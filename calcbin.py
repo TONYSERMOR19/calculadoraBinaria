4811ca1401f3d3ace3d86568fddb10a897574706
unknown <ericjaz1602@hotmail.com>
2023-06-15 18:56:27 -0600

    arreglando este ped

--- calcbin.py
+++ calcbin.py
@@ -28,4 +28,9 @@ def decimal_binario(numero):
         binario = str(residuo) + binario
         numero //= 2
     return binario
-
+def suma_binaria(binario1, binario2):
+    decimal1 = binario_decimal(binario1)
+    decimal2 = binario_decimal(binario2)
+    suma_decimal = decimal1 + decimal2
+    resultado_binario = decimal_binario(suma_decimal)
+    return resultado_binario
